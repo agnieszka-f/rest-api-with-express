fa456d5585783883236d07e376f6555ef78b7d70
Agnieszka Falandys <agnfal@gmail.com>
2021-10-12 11:25:59 +0200

    publication app

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -15,8 +15,16 @@ router.route('/seats/:id').get((req, res) => {
 });
 
 router.route('/seats').post((req, res) => {
-  seats.push({id: Date.now(), day: req.body.day, seat: req.body.seat, client: req.body.client, email: req.body.email });
-  res.json({message: 'OK'});
+  const {day, seat, client, email } = req.body;
+  
+  if(day && seat && client && email){
+	  
+    if(seats.some(el => el.day != day && el.seat != seat)){ 
+        seats.push({id: Date.now(), day, seat, client, email });
+        res.json({message: 'OK'});			
+    } else res.json({message: 'The slot is already taken...'});
+	
+  } else res.json({message: 'You have to fill all the fields...'});
 });
 
 router.route('/seats/:id').put((req, res) => {
